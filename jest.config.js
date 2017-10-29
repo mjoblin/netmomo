9dfe2ec7732716408471620a993072283b9d11fb
Michael Joblin <mjoblin@users.noreply.github.com>
2017-10-29 11:47:15 -0700

    Change babel and jest configs to get tests working

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,18 @@ module.exports = {
         "text-summary",
         "clover"
     ],
+    testPathIgnorePatterns: [
+        "/node_modules/",
+        "ignore",
+    ],
     transformIgnorePatterns: [
         "<rootDir>/node_modules/",
+    ],
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+        "\\.(css|scss|less)$": "<rootDir>/__mocks__/styleMock.js"
+    },
+    setupFiles: [
+        "<rootDir>/jestSetup.js",
     ],
 };
