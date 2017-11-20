b81aecafba69b0e7653c61f858c2902f99b313e0
Michael Joblin <mjoblin@users.noreply.github.com>
2017-11-19 16:31:56 -0800

    Remove unused actions and actionTypes imports from arp module

--- app/modules/arp/index.js
+++ app/modules/arp/index.js
@@ -1,9 +1,7 @@
-import * as actions from './actions';
-import * as actionTypes from './actionTypes';
 import * as components from './components';
 import * as constants from './constants';
 import reducer from './reducer';
 import * as selectors from './selectors';
 
 
-export default { actions, actionTypes, components, constants, reducer, selectors };
+export default { components, constants, reducer, selectors };
